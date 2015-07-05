6898b077695478900178f67f0d15bc38dfa39a80
Yurong Tao <yurong.tao@autodesk.com>
2015-07-05 14:08:33 +0800

    can receive and update the brownbag detail activity

--- code/android2/TCT/src/com/autodesk/tct/BrownbagDetailActivity.java
+++ code/android2/TCT/src/com/autodesk/tct/BrownbagDetailActivity.java
@@ -1,18 +1,41 @@
 package com.autodesk.tct;
 
+import com.autodesk.tct.brownbag.BrownBag;
+import com.autodesk.tct.server.ServerUtil;
+import com.autodesk.tct.server.ServerUtil.BrownbagDetailResponseHandler;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
-public class BrownbagDetailActivity extends AppCompatActivity{
+public class BrownbagDetailActivity extends AppCompatActivity implements BrownbagDetailResponseHandler{
 	private String brownbagId;
+	private static BrownBag currentBrownbag;
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bb_detail);
         Log.d("TCT", "Brownbag deatil activity started");
         Intent intent = getIntent();
-        brownbagId = intent.getStringExtra("id");
+        brownbagId = intent.getStringExtra("BROWNBAG_ID");
+        ServerUtil.setBrownbagDetailResponseHandler(this);
+        ServerUtil.getBrownbagDetail(brownbagId);
     }
+	
+	@Override
+	public void onBrownbagDetailReceivedSucceed(BrownBag brownbag) {
+		currentBrownbag = brownbag;
+		TextView titleTextView = (TextView) findViewById(R.id.bb_titile);
+		TextView descriptionTextView = (TextView) findViewById(R.id.bb_description);
+		titleTextView.setText(currentBrownbag.getTitle());
+		descriptionTextView.setText(currentBrownbag.getDescription());
+	}
+	
+	@Override
+	public void onBrownbagDetailReceivedFailed() {
+		// TODO Auto-generated method stub
+		
+	}
 }
